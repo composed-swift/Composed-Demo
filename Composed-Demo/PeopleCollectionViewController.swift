0f7eba71f489d985db91c1272efe3f256e3063a2
Shaps Benkau <shapsuk@me.com>
2019-07-24 13:34:35 +0100

    Storyboard and Estimated sizes are now support

--- Composed-Demo/PeopleCollectionViewController.swift
+++ Composed-Demo/PeopleCollectionViewController.swift
@@ -2,54 +2,13 @@ import UIKit
 import Composed
 import ComposedUI
 
-final class PeopleCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+final class PeopleCollectionViewController: UICollectionViewController {
 
     private var coordinator: CollectionCoordinator?
-
-    let people: [String] = [
-        "Anne",
-        "Shaps",
-        "Anton"
-    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         coordinator = CollectionCoordinator(collectionView: collectionView, sectionProvider: People.provider)
-        print(coordinator?.sections ?? [])
-
-        collectionView.register(UINib(nibName: "CollectionPersonCell", bundle: nil), forCellWithReuseIdentifier: "CollectionPersonCell")
-    }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return people.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionPersonCell", for: indexPath)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.bounds.width, height: 44)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: 0, height: 44)
     }
 
 }
