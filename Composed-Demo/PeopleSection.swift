9abb8cf9963c56eb91c428f07d0ddd19698ac9da
Shaps Benkau <shapsuk@me.com>
2019-07-24 10:19:14 +0100

    Implemented working table and collection based coordinators

--- Composed-Demo/PeopleSection.swift
+++ Composed-Demo/PeopleSection.swift
@@ -2,41 +2,6 @@ import UIKit
 import Composed
 import ComposedUI
 
-final class PeopleSection: ArraySection<String> {
-
-    let title: String
-
-    init(title: String, elements: [String]) {
-        self.title = title
-        super.init(elements: elements)
-    }
-
-}
-
-extension PeopleSection: TableSectionProvider {
-
-    // update header/footer to take an enum that's either a title or an element rather than a separate initializer
-
-    var tableSection: TableSection {
-        return TableSection(section: self, prototype: UITableViewCell(style: .default, reuseIdentifier: "UITableViewCell"), cellDequeueMethod: .class, cellConfigurator: { cell, index, section in
-            let person = section.element(at: index)
-            cell.textLabel?.text = person
-        }, header: title, footer: nil)
-    }
-
-}
-
-extension PeopleSection: CollectionSectionProvider {
-
-    var collectionSection: CollectionSection {
-        return CollectionSection(section: self, prototype: PersonCollectionCell.fromNib, cellDequeueMethod: .nib, cellConfigurator: { cell, index, section in
-            let person = section.element(at: index)
-            cell.titleLabel.text = person
-        })
-    }
-
-}
-
 final class People {
     static var provider: SectionProvider {
         let provider = ComposedSectionProvider()
@@ -54,4 +19,39 @@ final class People {
 
         return provider
     }
+}
+
+final class PeopleSection: ArraySection<String> {
+
+    let title: String
+
+    init(title: String, elements: [String]) {
+        self.title = title
+        super.init(elements: elements)
+    }
+
+}
+
+extension PeopleSection: TableSectionProvider {
+
+    // update header/footer to take an enum that's either a title or an element rather than a separate initializer
+
+    var tableSection: TableSection {
+        return TableSection(section: self, prototype: UITableViewCell(style: .default, reuseIdentifier: "UITableViewCell"), cellDequeueMethod: .class, cellConfigurator: { cell, index, section, _ in
+            let person = section.element(at: index)
+            cell.textLabel?.text = person
+        }, header: .title(title))
+    }
+
+}
+
+extension PeopleSection: CollectionSectionProvider {
+
+    var collectionSection: CollectionSection {
+        return CollectionSection(section: self, prototype: PersonCollectionCell.fromNib, cellDequeueMethod: .nib, cellConfigurator: { cell, index, section, context in
+            let person = section.element(at: index)
+            cell.titleLabel.text = person
+        })
+    }
+
 }
