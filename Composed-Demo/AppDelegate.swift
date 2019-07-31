abe33977b35b9288c31a658e01edaf83c0c1989a
Shaps Benkau <shapsuk@me.com>
2019-07-31 13:43:30 +0100

    Deployment target is now iOS 11 as expected For iOS 13, multiple windows support added Added more results to the list

--- Composed-Demo/AppDelegate.swift
+++ Composed-Demo/AppDelegate.swift
@@ -3,6 +3,9 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
+    @available(iOS 13, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
--- Composed-Demo/PeopleSection.swift
+++ Composed-Demo/PeopleSection.swift
@@ -6,16 +6,18 @@ final class People {
     static var provider: SectionProvider {
         let provider = ComposedSectionProvider()
 
-        provider.append(PeopleSection(title: "Family", elements: [
-            "Anne",
-            "Shaps",
-            "Anton",
-        ]))
+        for _ in 0..<10 {
+            provider.append(PeopleSection(title: "Family", elements: [
+                "Anne",
+                "Shaps",
+                "Anton",
+            ]))
 
-        provider.append(PeopleSection(title: "Friends", elements: [
-            "Joseph",
-            "Francesco",
-        ]))
+            provider.append(PeopleSection(title: "Friends", elements: [
+                "Joseph",
+                "Francesco",
+            ]))
+        }
 
         return provider
     }
