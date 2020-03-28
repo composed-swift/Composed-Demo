4c8a557fc8c05c3cbb57f52e7b4fcb65945b4f42
Shaps Benkau <shapsuk@me.com>
2020-03-28 23:04:28 +0000

    Added initial support for assets

--- Composed-Demo/Core/SceneDelegate.swift
+++ Composed-Demo/Core/SceneDelegate.swift
@@ -1,5 +1,15 @@
 import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
     var window: UIWindow?
+
+    @available(iOS 13.0, *)
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        #if DEBUG
+        (window?.rootViewController as? UITabBarController)?.selectedIndex = 3
+        #endif
+    }
+
 }
--- Composed-Demo/Media/MediaController.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-import ComposedMediaUI
-
-final class MediaController: MediaViewController {
-
-    
-}
