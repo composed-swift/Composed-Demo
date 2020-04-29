f52a8ae0b6ce229de71d7d6839349b20d7a0afff
Shaps Benkau <shapsuk@me.com>
2020-04-30 00:28:27 +0100

    Drag and drop is now propagated through the original delegate when its not implemented by the section

--- Composed-Demo/Collection/PeopleCollectionViewController.swift
+++ Composed-Demo/Collection/PeopleCollectionViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Composed
 import ComposedUI
 
-final class PeopleCollectionViewController: PeopleViewController {
+final class PeopleCollectionViewController: PeopleViewController, UICollectionViewDelegate {
 
     @IBOutlet private weak var collectionView: UICollectionView!
     private var coordinator: CollectionCoordinator?
@@ -10,12 +10,29 @@ final class PeopleCollectionViewController: PeopleViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         coordinator = CollectionCoordinator(collectionView: collectionView, sectionProvider: provider)
+        collectionView.delegate = self
     }
 
     /// Required specifically for `UICollectionViewFlowLayout` since some rotations don't cause an invalidation!?
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         self.coordinator?.invalidateLayout()
+    }
+
+}
+
+extension PeopleCollectionViewController: UICollectionViewDropDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        return UICollectionViewDropProposal(operation: .copy)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return true
     }
 
 }
